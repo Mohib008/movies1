e614058f659046774d801d530e0f85cb2004604b
Mohib Arsala <mohib@Mohibs-MacBook-Air.local>
2025-07-02 12:55:47 -0400

    updates from Macbook air

--- app/components/Navbar/navbar.jsx
+++ app/components/Navbar/navbar.jsx
@@ -5,7 +5,12 @@ import Search from "./search";
 import NumResults from "./numResults";
 // Navbar component that combines Logo, Input, and NumResults components
 
-function Navbar({ children }) {
+// Movie type definition removed because type aliases are not allowed in .jsx files.
+
+/**
+ * @param {{ children: React.ReactNode, movies: Movie[] }} props
+ */
+function Navbar({ children, movies }) {
   return (
     <div>
       <nav className="nav-bar">{children}</nav>
