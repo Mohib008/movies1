e0acc11fb5e2423225d4ed984c4b2c53faaff409
mohib <mohib.0008@gmail.com>
2025-07-04 21:36:43 -0400

    in box components the children props change to elements and back reversed to children it is just for exercises

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
   const [watched] = useState(tempWatchedData);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Navbar movies={tempMovieData}>
+      <Navbar movies={movies}>
         <Logo />
         <Search />
-        <NumResults movies={tempWatchedData} />
+        <NumResults movies={movies} />
       </Navbar>
       <main className="main">
         <Box>
