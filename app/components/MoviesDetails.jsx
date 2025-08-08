28edb0043839a76d865accb6cbc1704367f24be1
mohib <mohib.0008@gmail.com>
2025-08-08 15:17:38 -0400

    moviesDetails are updated, useEffect added

--- app/components/MoviesDetails.jsx
+++ app/components/MoviesDetails.jsx
@@ -1,7 +1,22 @@
 "use client";
-import React from "react";
+import React, { use } from "react";
+import { useEffect, useState } from "react";
+
+const KEY = "9001e482";
 
 export default function MoviesDetails({ selectedId, onClose }) {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const getMovieDetails = async () => {
+      const res = await fetch(
+        `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+      );
+      const data = await res.json();
+      console.log(data);
+    };
+    getMovieDetails();
+  }, []);
+
   return (
     <div className="details">
       {selectedId}
