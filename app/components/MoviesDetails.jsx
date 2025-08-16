3d3fe844cc7183ac188093995669f91fe9bf1f82
mohib <mohib.0008@gmail.com>
2025-08-16 13:32:26 -0400

    new updates in rating componets

--- app/components/MoviesDetails.jsx
+++ app/components/MoviesDetails.jsx
@@ -2,11 +2,13 @@
 import React, { use } from "react";
 import { useEffect, useState } from "react";
 import Rating from "./Box/Rating";
+import Loading from "./loading";
 
 const KEY = "9001e482";
 
 export default function MoviesDetails({ selectedId, onClose }) {
   const [movies, setMovies] = useState({});
+  const [loading, setLoading] = useState(false);
 
   const {
     Title: title,
@@ -29,43 +31,51 @@ export default function MoviesDetails({ selectedId, onClose }) {
 
   useEffect(() => {
     const getMovieDetails = async () => {
+      setLoading(true);
       const res = await fetch(
         `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
       );
       const data = await res.json();
       setMovies(data);
+      setLoading(false);
     };
     getMovieDetails();
   }, [selectedId]);
 
   return (
     <div className="details">
-      <header>
-        <button className="btn-back" onClick={onClose}>
-          &larr;
-        </button>
-        <img src={poster} alt={`Poster of ${movies} movie`} />
-        <div className="details-overview">
-          <h2>{title}</h2>
-          <span>
-            {released} &bull;🕒 {runtime}
-          </span>
-          <span>⭐ {imdbRating}</span>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <header>
+            <button className="btn-back" onClick={onClose}>
+              &larr;
+            </button>
+            <img src={poster} alt={`Poster of ${movies} movie`} />
+            <div className="details-overview">
+              <h2>{title}</h2>
+              <span>
+                {released} &bull;🕒 {runtime}
+              </span>
+              <span>⭐ {imdbRating}</span>
 
-          <span>🔖 {genre}</span>
-        </div>
-      </header>
-      <section className="details-info">
-        <div className="rating">
-          <Rating />
-        </div>
-        <p>
-          💬 <em>{plot}</em>
-        </p>
-        <p>👥 Starring: {actors}</p>
-        <p>🎬 Directed by: {director}</p>
-      </section>
-      <div>{selectedId}</div>
+              <span>🔖 {genre}</span>
+            </div>
+          </header>
+          <section className="details-info">
+            <div className="rating">
+              <Rating />
+            </div>
+            <p>
+              💬 <em>{plot}</em>
+            </p>
+            <p>👥 Starring: {actors}</p>
+            <p>🎬 Directed by: {director}</p>
+          </section>
+          <div>{selectedId}</div>
+        </>
+      )}
     </div>
   );
 }
